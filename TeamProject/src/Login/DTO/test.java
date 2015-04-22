0a98a8edad7890631fcdadc7b5a2de47bfd5fa0c
kjh <kjh@naver.com>
2015-04-22 14:09:31 +0900

    지현 회우너수정

--- TeamProject/src/Login/DTO/test.java
+++ TeamProject/src/Login/DTO/test.java
@@ -1,5 +1,14 @@
 package Login.DTO;
 
 public class test {
-
+	private String id;//ID
+	private String pw;//암호
+	private String name;//이름
+	private String jumin;//주민번호
+	private String phone;//휴대폰
+	private String nick;//닉네임
+	private String email;//이메일
+	private int gender;//성별
+	private int age;//나이
+	
 }
